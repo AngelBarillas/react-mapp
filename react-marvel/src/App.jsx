c02f1c398ac9d05c2686ee4cee1af52d3592d162
Angel Barillas <a.e.barillas.ablan@gmail.com>
2024-10-07 10:04:15 -0400

    changed initial useEffect to only contain the function call and saved loader in component (Csar)

--- react-marvel/src/App.jsx
+++ react-marvel/src/App.jsx
@@ -6,6 +6,7 @@ import Title from "./Title";
 import List from "./List";
 import Hero from "./assets/Hero";
 import { fetch_status } from "./assets/fetchstatus";
+import Loader from "./Loader";
 
 const App = function () {
   const [characters, setCharacters] = useState([]);
@@ -17,36 +18,37 @@ const App = function () {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (offset === 0) {
-        setStatus(fetch_status.loading);
-      }
-      const response = await fetch(
-        `https://gateway.marvel.com/v1/public/characters?offset=${offset}&apikey=c5b21bcd83f9cb497db19d4a57fb1837`
-      );
-
-      try {
-        const apiData = await response.json();
-        if (offset > 0) {
-          const apiChar = characters.concat(apiData.data.results);
-          setCharacters(apiChar);
-          setStatus(fetch_status.success);
-          console.log(apiChar);
-        } else {
-          const apiChar = apiData.data.results;
-          setCharacters(apiChar);
-          setStatus(fetch_status.success);
-          console.log(offset);
-        }
-      } catch (error) {
-        setStatus(fetch_status.error);
-        console.log(error);
-      }
-    };
     fetchData();
   }, [offset]);
 
-  if (status === fetch_status.loading) return <div className="loader"></div>;
+  const fetchData = async () => {
+    if (offset === 0) {
+      setStatus(fetch_status.loading);
+    }
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?offset=${offset}&apikey=c5b21bcd83f9cb497db19d4a57fb1837`
+    );
+
+    try {
+      const apiData = await response.json();
+      if (offset > 0) {
+        const apiChar = characters.concat(apiData.data.results);
+        setCharacters(apiChar);
+        setStatus(fetch_status.success);
+        console.log(apiChar);
+      } else {
+        const apiChar = apiData.data.results;
+        setCharacters(apiChar);
+        setStatus(fetch_status.success);
+        console.log(offset);
+      }
+    } catch (error) {
+      setStatus(fetch_status.error);
+      console.log(error);
+    }
+  };
+
+  if (status === fetch_status.loading) return <Loader />;
 
   if (status === fetch_status.success) {
     return (
---
+++ react-marvel/src/Loader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./App.css";
+
+const Loader = function () {
+  return <div className="loader"></div>;
+};
+export default Loader;
